3d02d333e70fef4870713ace1ad635bdae73713b
LocustSwarm52 <nickstarr808@gmail.com>
2024-04-13 19:12:15 -0600

    Setup Node.js server for Heroku deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'Public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'Public/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
